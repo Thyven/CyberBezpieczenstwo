bccfaf2e024ae7b2d7ef57e9e6d024e3a8069fea
mikeymike6 <siwiec.mikolaj@gmail.com>
2022-11-06 12:51:47 +0100

    OneTimePassword

---
+++ CyberBezpieczenstwo/Data/GeneratePassword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberBezpieczenstwo.Data
+{
+    public class GeneratePassword
+    {
+        public string OneTimePassword(int a)
+        {
+            var rnd = new Random();
+            int x = rnd.Next(100);
+            if (x == 0) x = 1;
+            decimal newPassword = Decimal.Divide(a, x);
+            newPassword = Math.Round(newPassword, 7);
+            return newPassword.ToString();
+        }
+    }
+}
